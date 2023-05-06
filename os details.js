431c890dc68f91bec74873a8224bb1f445a62ec2
Tanmay Agarwal <84774373+tanmay8433@users.noreply.github.com>
2023-05-06 10:29:52 +0530

    Add files via upload

---
+++ os details.js
@@ -0,0 +1,5 @@
+// how do you find operating system details ?
+
+//console.log(navigator.platform);
+// window.navigator object contains information about the visitor browers os details. 
+// and some of  the os properties are available under platform property. 
---
+++ submit form using js.js
@@ -0,0 +1,6 @@
+// how do you submit a form using js?
+// you can submit form using document.forms[].submit()
+// function submit(){
+//     document.forms[0].submit()
+// }
+// submit()
